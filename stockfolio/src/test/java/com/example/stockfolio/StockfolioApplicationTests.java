7361f26190c75db96fd5ab73bc4b6d8f924c9096
bhr469@myy.haaga-helia.fi <bhr469@myy.haaga-helia.fi>
2026-01-22 13:21:01 +0200

    SpringBoot projekti luotu

---
+++ stockfolio/src/test/java/com/example/stockfolio/StockfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stockfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
