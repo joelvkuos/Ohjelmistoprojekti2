de065aabc7e400efa399b29c27ed2fb58c12316f
bhq638@myy.haaga-helia.fi <bhq638@myy.haaga-helia.fi>
2026-01-29 12:54:27 +0200

    Model luokat

--- stockfolio/src/main/java/com/example/stockfolio/model/Portfolio.java
+++ stockfolio/src/main/java/com/example/stockfolio/model/Portfolio.java
@@ -1,5 +1,40 @@
 package com.example.stockfolio.model;
 
+import jakarta.persistence.Entity;
+
+@Entity
 public class Portfolio {
+
+    private Long portfolio_id;
+    private Long app_user_id;
+    private String portfolio_name;
+
+    public Portfolio () {
+
+    }
+    
+    public Portfolio(Long portfolio_id, Long app_user_id, String portfolio_name) {
+        this.portfolio_id = portfolio_id;
+        this.app_user_id = app_user_id;
+        this.portfolio_name = portfolio_name;
+    }
+    public Long getPortfolio_id() {
+        return portfolio_id;
+    }
+    public void setPortfolio_id(Long portfolio_id) {
+        this.portfolio_id = portfolio_id;
+    }
+    public Long getApp_user_id() {
+        return app_user_id;
+    }
+    public void setApp_user_id(Long app_user_id) {
+        this.app_user_id = app_user_id;
+    }
+    public String getPortfolio_name() {
+        return portfolio_name;
+    }
+    public void setPortfolio_name(String portfolio_name) {
+        this.portfolio_name = portfolio_name;
+    }
 
 }
--- stockfolio/src/main/java/com/example/stockfolio/model/User.java
+++ stockfolio/src/main/java/com/example/stockfolio/model/User.java
@@ -1,5 +1,60 @@
 package com.example.stockfolio.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="app_user")
 public class User {
+
+    private Long app_user_id;
+    private String userName;
+    private String passwordHash;
+    private String email;
+    private String phone;
+        
+    public User () {
+        
+    }
+
+    public User(Long app_user_id, String userName, String passwordHash, String email, String phone) {
+        this.app_user_id = app_user_id;
+        this.userName = userName;
+        this.passwordHash = passwordHash;
+        this.email = email;
+        this.phone = phone;
+    }
+    
+    public Long getApp_user_id() {
+        return app_user_id;
+    }
+    public void setApp_user_id(Long app_user_id) {
+        this.app_user_id = app_user_id;
+    }
+    public String getUserName() {
+        return userName;
+    }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
 
 }
