adf597e92b2274368d2d49f6615f79f9a76edf25
Konsta Lyytikäinen <konsta.lyytikainen@gmail.com>
2026-02-05 13:25:57 +0200

    User controlleri

--- stockfolio/src/main/java/com/example/stockfolio/controller/AuthRestController.java
+++ stockfolio/src/main/java/com/example/stockfolio/controller/AuthRestController.java
@@ -1,5 +1,14 @@
 package com.example.stockfolio.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.stockfolio.service.JwtService;
+
+@RestController
+@RequestMapping("/api/auth")
 public class AuthRestController {
+
+    private JwtService jwtService;
 
 }
