aa0b7b15a6f44db5e8d7bc46d40db2e5fe71c361
bhq638@myy.haaga-helia.fi <bhq638@myy.haaga-helia.fi>
2026-02-05 21:57:22 +0200

    Portfolio service luokka

--- stockfolio/src/main/java/com/example/stockfolio/service/PortfolioService.java
+++ stockfolio/src/main/java/com/example/stockfolio/service/PortfolioService.java
@@ -1,9 +1,16 @@
 package com.example.stockfolio.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.example.stockfolio.model.Portfolio;
 import com.example.stockfolio.repository.PortfolioRepository;
+import com.example.stockfolio.model.User;
 
 @Service
 public class PortfolioService {
@@ -13,5 +20,44 @@ public class PortfolioService {
 
     @Autowired
     private UserService userService;
+
+    /*Etsii kaikki portfoliot */
+    public List<Portfolio> findAllPortfolios(){
+        return portfolioRepository.findAll();
+    }
+
+    /*Etsii yhden portfolion sen ID:llä */
+    public Optional<Portfolio> findPortfolioById(Long portfolioId){
+        return portfolioRepository.findById(portfolioId);
+    }
+
+    /*Etsii käyttäjän portfoliot */
+    public List<Portfolio> getUsersPortfolios(){
+        User user = userService.getAuthenticatedUser()
+            .orElseThrow(() -> new ResponseStatusException(HttpStatus.UNAUTHORIZED));
+        return portfolioRepository.findByApp_user_id(user.getApp_user_id());
+    }
+
+    /*Luo portfolion */
+    public Portfolio createPortfolio(Portfolio portfolio){
+        User user = userService.getAuthenticatedUser()
+            .orElseThrow(() -> new ResponseStatusException(HttpStatus.UNAUTHORIZED));
+            portfolio.setApp_user_id(user.getApp_user_id());
+            return portfolioRepository.save(portfolio);
+    }
+
+    /*Poistaa portfolion (jos käyttäjällä oikeus) */
+    public void delete(Long portfolioId){
+        Portfolio portfolio = portfolioRepository.findById(portfolioId)
+            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        User user = userService.getAuthenticatedUser()
+            .orElseThrow(() -> new ResponseStatusException(HttpStatus.UNAUTHORIZED));
+
+        if(!portfolio.getApp_user_id().equals(user.getApp_user_id())){
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Not your portfolio");
+        }
+
+        portfolioRepository.deleteById(portfolioId);
+    }
 
 }
