d0d6f397885bc47028ad8b20cda94024c9afb656
bhr469@myy.haaga-helia.fi <bhr469@myy.haaga-helia.fi>
2026-02-12 13:47:21 +0200

    Footer.tsx and footer.css added

--- frontend/src/pages/landingPage.tsx
+++ frontend/src/pages/landingPage.tsx
@@ -1,5 +1,6 @@
 import "../styles/landingPage.css"
 import NavBar from "../components/AppBar";
+import Footer from "../components/Footer";
 import stocksImage from "../assets/Stocks.jpg"
 
 
@@ -10,13 +11,14 @@ export default function LandingPage() {
             <NavBar />
             <div className="column-section">
                 <div className="column-text">
-                        <h1>Create your own stock portfolio</h1>
-                        <h4>Browse other stock portfolios and rate them.</h4>
+                    <h1>Create your own stock portfolio</h1>
+                    <h4>Browse other stock portfolios and rate them.</h4>
                     <div>
-                            <img className="stocksimg FadeIn" src={stocksImage}></img>
+                        <img className="stocksimg FadeIn" src={stocksImage}></img>
                     </div>
                 </div>
             </div>
+            <Footer />
         </>
     );
 }
