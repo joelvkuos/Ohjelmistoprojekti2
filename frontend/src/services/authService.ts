99c9d1bc2e74c267018c32321144c67907cd94bd
bhr469@myy.haaga-helia.fi <bhr469@myy.haaga-helia.fi>
2026-03-10 21:30:02 +0200

    Aloitettu POST-pyynnöt register sivulle

--- frontend/src/services/authService.ts
+++ frontend/src/services/authService.ts
@@ -1,1 +1,17 @@
 /** Auth-funktiot */
+/** eli sisältää register() <- POST /api/users/register */
+/** login() <- POST /api/auth/login (tulevaisuudessa) */
+/** logout() */
+
+import type { Customer } from '../types/api.types'
+
+const API_URL = 'https://stockfolio-postgres-stockfolio-postgres.2.rahtiapp.fi/api'
+
+export const addCustomer = async (customer: Omit<Customer, '_links'>): Promise<Customer> => {
+    const response = await fetch(`${API_URL}/register`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(customer),
+    });
+    return response.json();
+}
