55aa7ebeec762ba8b895f5f4eb8ed08c85236fb2
bhq631@myy.haaga-helia.fi <bhq631@myy.haaga-helia.fi>
2026-03-20 13:05:31 +0200

    Homepage layout done

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -3,19 +3,26 @@ import "../styles/homePage.css"
 
 import Navigation from "../components/Navigation";
 import { Button } from "@mui/material";
+import stocksImage from "../assets/Stocks.jpg"
 
 
 export default function Homepage() {
     return (
         <>
             <Navigation />
-             <div className="column-section">
+             <div className="two-column-section">
                 <div className="column-text">
                     <h1>Welcome User!</h1>
-                    <h4>Start exploring</h4>
+                    <h4>Welcome to Stockfolio — your personal space to showcase and track your investment journey. 
+                        Build your own stock portfolios, monitor your performance, and see your investments come to life in one place. 
+                        Explore what others are investing in, share your thoughts, 
+                        and join the conversation — but most importantly, keep your own portfolio front and center.</h4>
                     <Button className="btn-starter">
                         Get Started With Stockfolio
                     </Button>
+                </div>
+                <div className="column-image">
+                    <img className="stocksimg FadeIn" src={stocksImage}></img>
                 </div>
             </div>
             <Footer />
