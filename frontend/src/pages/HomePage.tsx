a198765549647924621feeecac17d357f64314d2
bhq631@myy.haaga-helia.fi <bhq631@myy.haaga-helia.fi>
2026-03-20 12:46:07 +0200

    Homepage button

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -2,14 +2,21 @@ import Footer from "../components/Footer";
 import "../styles/homePage.css"
 
 import Navigation from "../components/Navigation";
+import { Button } from "@mui/material";
 
 
 export default function Homepage() {
     return (
         <>
             <Navigation />
-            <div>
-
+             <div className="column-section">
+                <div className="column-text">
+                    <h1>Welcome User!</h1>
+                    <h4>Start exploring</h4>
+                    <Button className="btn-starter">
+                        Get Started With Stockfolio
+                    </Button>
+                </div>
             </div>
             <Footer />
 
