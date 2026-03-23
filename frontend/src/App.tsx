48ea740f51ae6e3faf1fd32bc94178034c411234
bhq631@myy.haaga-helia.fi <bhq631@myy.haaga-helia.fi>
2026-03-23 13:15:57 +0200

    News page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import Login from './pages/Login'
 import Register from './pages/Register'
 import LandingPage from './pages/landingPage'
 import Homepage from './pages/HomePage'
+import News from './pages/News'
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
       <Route path='/login' element={<Login />} />
       <Route path='/register' element={<Register />} />
       <Route path='/homepage' element={<Homepage />} />
+      <Route path='/news' element={<News />} />
     </Routes>
   )
 }
---
+++ frontend/src/pages/News.tsx
@@ -0,0 +1,15 @@
+import Footer from "../components/Footer";
+import Navigation from "../components/Navigation";
+
+export default function News() {
+    return (
+    <>
+    <Navigation />
+        <div>
+            <h1>News</h1>
+            <p>Here you can find the latest news about stocks and investments.</p>
+        </div>
+    <Footer />
+    </>
+    )
+}
