0f875cc03d3b09fd3a8fdfc62b92c8fc063ba3a5
bhq631@myy.haaga-helia.fi <bhq631@myy.haaga-helia.fi>
2026-04-02 12:59:35 +0300

    Tehty navigointi muutos ja profile sivu

---
+++ frontend/src/pages/Profile.tsx
@@ -0,0 +1,17 @@
+
+import Navigation from "../components/Navigation";
+import Footer from "../components/Footer";
+
+
+export default function Profile() {
+    return (
+    <>
+    <Navigation />
+    <div>
+        <h1>Profile Page</h1>
+        <p>This is where user profile information will be displayed.</p>
+    </div>
+    <Footer />
+    </>
+    );
+}
