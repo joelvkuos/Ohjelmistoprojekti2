e1708c5903c83c63304adcc70edd82884af2d7b1
bhq631@myy.haaga-helia.fi <bhq631@myy.haaga-helia.fi>
2026-04-16 13:06:50 +0300

    navi ja footer

--- frontend/src/pages/Stocks.tsx
+++ frontend/src/pages/Stocks.tsx
@@ -1,10 +1,15 @@
-
+import Navigation from '../components/Navigation'
+import Footer from '../components/Footer'
 
 export default function Stocks() {
     return (
+    <>
+    <Navigation />
         <div>
             <h1>Stocks Page</h1>
             <p>This is where stock information will be displayed.</p>
         </div>
+    <Footer />
+    </>
     );
 }
