070e50fa5736d49a5a07f5d2cd244f5807e44764
toom-kaarel.kiisk@myy.haaga-helia.fi <toom-kaarel.kiisk@myy.haaga-helia.fi>
2026-04-20 14:48:23 +0300

    backend fetch eager

--- stockfolio/src/main/java/com/example/stockfolio/model/Portfolio.java
+++ stockfolio/src/main/java/com/example/stockfolio/model/Portfolio.java
@@ -9,6 +9,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.FetchType;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
@@ -27,7 +28,7 @@ public class Portfolio {
     @NotBlank(message = "Portfolio name is required")
     private String portfolioName;
 
-    @OneToMany(mappedBy = "portfolio", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "portfolio", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
     private List<Holdings> holdings;
 
     public Portfolio () {
